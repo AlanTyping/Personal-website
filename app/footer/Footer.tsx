9141972bca5a8dc5123f2baef3f899099aa9669e
AlanTyping <alan.a.n.r.ar@gmail.com>
2023-12-13 14:42:45 -0300

    adding network icons

--- app/footer/Footer.tsx
+++ app/footer/Footer.tsx
@@ -7,11 +7,11 @@ import Link from 'next/link';
 
 export default function Footer() {
   return (
-    <div className='relative flex items-center justify-center w-full h-[50px] bg-[#00152b]'>
-      <ul className="flex w-[45%] absolute top-4 items-center justify-evenly">
-        <li><Link href={''} target='blank'><Image src={whatsapp} alt='' className="h-[25px] w-[25px]" /></Link></li>
-        <li><Link href={'https://www.instagram.com/alan_anr/'} target='blank'><Image src={instagram} alt='' className="h-[25px] w-[25px]" /></Link></li>
-        <li><Link href={'mailto:alan.a.n.r@gmail.com'} target='blank'><Image src={mail} alt='' className="h-[25px] w-[25px]" /></Link></li>
+    <div className='relative flex items-center justify-center w-full h-[100px] 2xl:h-[150px] bg-[#00152b]'>
+      <ul className="flex w-[45%] absolute items-center justify-evenly">
+        <li><Link href={'https://api.whatsapp.com/send?phone=541138235395'} target='blank'><Image src={whatsapp} alt='' className="h-[25px] w-[25px] md:h-[30px] md:w-[30px] 2xl:h-[45px] 2xl:w-[45px]" /></Link></li>
+        <li><Link href={'https://www.instagram.com/alan_anr/'} target='blank'><Image src={instagram} alt='' className="h-[25px] w-[25px] md:h-[30px] md:w-[30px] 2xl:h-[45px] 2xl:w-[45px]" /></Link></li>
+        <li><Link href={'mailto:alan.a.n.r.ar@gmail.com'} target='blank'><Image src={mail} alt='' className="h-[25px] w-[25px] md:h-[30px] md:w-[30px] 2xl:h-[45px] 2xl:w-[45px]" /></Link></li>
       </ul>
     </div>
   )
