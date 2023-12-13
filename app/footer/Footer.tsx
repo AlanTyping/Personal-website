db321787f1a0d8c7a584e0f70631132b75f7b463
AlanTyping <alan.a.n.r.ar@gmail.com>
2023-12-13 14:48:49 -0300

    adding icons

--- app/footer/Footer.tsx
+++ app/footer/Footer.tsx
@@ -8,10 +8,10 @@ import Link from 'next/link';
 export default function Footer() {
   return (
     <div className='relative flex items-center justify-center w-full h-[100px] 2xl:h-[150px] bg-[#00152b]'>
-      <ul className="flex w-[45%] absolute items-center justify-evenly">
-        <li><Link href={'https://api.whatsapp.com/send?phone=541138235395'} target='blank'><Image src={whatsapp} alt='' className="h-[25px] w-[25px] md:h-[30px] md:w-[30px] 2xl:h-[45px] 2xl:w-[45px]" /></Link></li>
-        <li><Link href={'https://www.instagram.com/alan_anr/'} target='blank'><Image src={instagram} alt='' className="h-[25px] w-[25px] md:h-[30px] md:w-[30px] 2xl:h-[45px] 2xl:w-[45px]" /></Link></li>
-        <li><Link href={'mailto:alan.a.n.r.ar@gmail.com'} target='blank'><Image src={mail} alt='' className="h-[25px] w-[25px] md:h-[30px] md:w-[30px] 2xl:h-[45px] 2xl:w-[45px]" /></Link></li>
+      <ul className="flex w-[80%] md:w-[45%] absolute items-center justify-evenly">
+        <li><Link href={'https://api.whatsapp.com/send?phone=541138235395'} target='blank'><Image src={whatsapp} alt='' className="h-[30px] w-[30px] md:h-[30px] md:w-[30px] 2xl:h-[45px] 2xl:w-[45px]" /></Link></li>
+        <li><Link href={'https://www.instagram.com/alan_anr/'} target='blank'><Image src={instagram} alt='' className="h-[30px] w-[30px] md:h-[30px] md:w-[30px] 2xl:h-[45px] 2xl:w-[45px]" /></Link></li>
+        <li><Link href={'mailto:alan.a.n.r.ar@gmail.com'} target='blank'><Image src={mail} alt='' className="h-[30px] w-[30px] md:h-[30px] md:w-[30px] 2xl:h-[45px] 2xl:w-[45px]" /></Link></li>
       </ul>
     </div>
   )
