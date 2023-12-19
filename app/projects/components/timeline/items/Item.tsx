723be26eb96bbd3e9d0aef0d9f3206ae897be813
AlanTyping <alan.a.n.r.ar@gmail.com>
2023-12-19 14:59:12 -0300

    adding scroll animation

--- app/projects/components/timeline/items/Item.tsx
+++ app/projects/components/timeline/items/Item.tsx
@@ -2,6 +2,8 @@ import { StaticImageData } from 'next/image';
 import Image from 'next/image';
 import Link from 'next/link';
 import { poppins500 } from '../../../../utilities/fonts';
+import { useEffect, useRef, useState } from 'react';
+import { motion, useAnimation } from 'framer-motion';
 
 type Props = {
   name: string
@@ -14,8 +16,38 @@ type Props = {
 }
 
 export const Item = ({ name, descripcion, icon, date, link, image, color }: Props) => {
+  const animatedElementRef = useRef(null);
+  const [visible, setIsVisible] = useState<boolean>(false);
+  const [background, setbackground] = useState<string>('');
+  const controls = useAnimation();
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            controls.start({ x: 0, opacity: 1 }); // Agregar clase de animación al entrar en la vista
+          } else {
+             // Eliminar clase de animación al salir de la vista
+          }
+        });
+      },
+      { threshold: 0.5 } // Umbral del 50% de visibilidad
+    );
+
+    if (animatedElementRef.current) {
+      observer.observe(animatedElementRef.current);
+    }
+
+    return () => {
+      if (animatedElementRef.current) {
+        observer.unobserve(animatedElementRef.current);
+      }
+    };
+  }, []);
+
   return (
-    <li>
+    <li ref={animatedElementRef}>
       <div className="h-auto py-10 2xl:py-14 w-[98vw] lg:w-[90vw] flex flex-row justify-center items-center relative">
         <div className="h-full w-[10%] absolute left-0">
           <div className='w-full h-full relative flex-center flex-col items-center'>
@@ -27,8 +59,12 @@ export const Item = ({ name, descripcion, icon, date, link, image, color }: Prop
           </div>
         </div>
 
-        <div className="h-auto w-full flex-center justify-center rounded mt-8 2xl:mt-14">
-          <div className="bg-[#012979] overflow-hidden w-[80%] rounded flex items-center p-4 sm:p-6 2xl:p-8 relative flex-col border-b-[6px] border-[#ffa500]">
+        <motion.div
+          initial={{ x: -100, opacity: 0 }} // Estado inicial fuera de la vista
+          animate={controls} // Animación controlada por Framer Motion
+          transition={{ duration: 0.2, ease: 'easeOut' }}
+          className="h-auto w-full flex-center justify-center rounded mt-8 2xl:mt-14">
+          <div className={`bg-[#012979] overflow-hidden w-[80%] rounded flex items-center p-4 sm:p-6 2xl:p-8 relative flex-col border-b-[6px] border-[#ffa500] ${visible ? 'bg-red-400' : ''}`}>
             <Image src={image} alt='' className='hidden lg:flex w-[150%] brightness-[30%] top-0 h-auto rounded absolute' style={{ objectFit: 'cover' }} />
             <div className={`hidden lg:flex absolute inset-x-0 inset-y-0 z-[1] ${color}`} />
 
@@ -51,9 +87,8 @@ export const Item = ({ name, descripcion, icon, date, link, image, color }: Prop
                 <Link href={link} target='blank' className={`p-2 bg-[#001d3d] lg:bg-[#0040c0] sm:text-[1.1rem] 2xl:text-[1.5rem] px-4 2xl:px-6 text-white rounded ${poppins500.className}`}>visit site</Link>
               </div>
             </div>
-
           </div>
-        </div>
+        </motion.div>
       </div>
     </li>
   )
