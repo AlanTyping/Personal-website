0e16b9a7142a4ddd6165cd5d3faa456384b82c9b
AlanTyping <alan.a.n.r.ar@gmail.com>
2023-12-21 16:05:38 -0300

    .

--- app/presentation/page.tsx
+++ app/presentation/page.tsx
@@ -68,7 +68,7 @@ export default function Presentation() {
                     animate={{ x: 0, scale: 1, opacity: 1 }}
                     transition={{ duration: 0.2 }}
                     className={"w-[85%] sm:w-[90%] mt-2 2xl:mt-10 h-[75px] flex justify-start items-center"}>
-                    <h2 className={`text-[1.8rem] 2xl:text-[2.2rem] text-[#ffa500] border-b-[1px] border-[#ffa500] h-auto ${poppins500.className}`}>About</h2>
+                    <h2 className={`text-[1.8rem] 2xl:text-[2.25rem] text-[#ffa500] border-b-[1px] border-[#ffa500] h-auto ${poppins500.className}`}>About</h2>
                   </motion.div>
 
                   <motion.p
