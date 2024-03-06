98703f3e7258e40490fb2f1b46aaa7fd580e80c5
Alan.jpg <alan.a.n.r.ar@gmail.com>
2024-03-06 13:38:54 -0300

    .

--- app/components/SchoolSection.tsx
+++ app/components/SchoolSection.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 
 export default function SchoolSection() {
   return (
-    <div className="py-16 text-[0.95rem] md:text-[1.1rem] flex-center flex-col justify-center w bg-[var(--darkBlue)] border-t-[1px] border-[#ffa500] text-white">
+    <div className="py-16 2xl:py-20 text-[0.95rem] md:text-[1.1rem] 2xl:text-[1.5rem] flex-center flex-col justify-center w bg-[var(--darkBlue)] border-t-[1px] border-[#ffa500] text-white">
       <p className='md:w-[60%] w-[80%]'>
         Actualmente decidí especializarme en el desarrollo de páginas
         web para instituciones educativas 🏫, por lo que, si formas parte
@@ -12,7 +12,7 @@ export default function SchoolSection() {
         que ya tiene, quizás te sientas interesado.
       </p>
       <div className='md:w-[60%] w-[80%] mt-4'>
-        <span>Para más info, consultar el siguiente pdf haciendo <a className='text-[var(--lightBlue)] border-b-[1px] border-[var(--lightBlue)]' href="./a.pdf" download>click aquí</a></span>
+        <span>Para más info, consultar el siguiente pdf haciendo <a className='text-[var(--lightBlue)] border-b-[1px] border-[var(--lightBlue)]' href="" >click aquí</a></span>
       </div>
     </div>
   )
