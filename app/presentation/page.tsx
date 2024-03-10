d93c7df0af2cbf14bf20458d709aca933968a80c
Alan.jpg <alan.a.n.r.ar@gmail.com>
2024-03-10 15:08:50 -0300

    .

--- app/presentation/page.tsx
+++ app/presentation/page.tsx
@@ -68,14 +68,14 @@ export default function Presentation() {
                     animate={{ x: 0, scale: 1, opacity: 1 }}
                     transition={{ duration: 0.2 }}
                     className={"w-[85%] sm:w-[90%] mt-2 2xl:mt-10 h-[75px] flex justify-start items-center"}>
-                    <h2 className={`text-[1.8rem] 2xl:text-[2.25rem] text-[#ffa500] border-b-[1px] border-[#ffa500] h-auto ${poppins500.className}`}>About</h2>
+                    <h2 className={`text-[1.8rem] 2xl:text-[2.25rem] text-[#ffa500] border-b-[1px] border-[#ffa500] h-auto ${poppins500.className}`}>Acerca de mí</h2>
                   </motion.div>
 
                   <motion.p
                     initial={{ opacity: 0 }}
                     animate={{ x: 0, scale: 1, opacity: 1 }}
                     transition={{ duration: 0.3 }}
-                    className={"w-[80%] sm:w-[85%] mt-3 sm:mt-4 2xl:mt-12"}>
+                    className={"w-[80%] sm:w-[85%] mt-3 sm:mt-3 2xl:mt-12"}>
                       Desarrollador de Argentina, actualmente 19 años y dispuesto a enfrentar cualquier reto
                       a cambio de aumentar mis conocimietos y habilidades.
                   </motion.p>
