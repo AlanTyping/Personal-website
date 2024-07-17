8bc55c0c9d2c5172b6db372f6626179735057b99
AlanTyping <alan.a.n.r.ar@gmail.com>
2024-07-17 15:19:43 -0300

    .

--- app/blog/Blog.tsx
+++ app/blog/Blog.tsx
@@ -26,7 +26,7 @@ export default async function Blog() {
         <h1 className="font-bold text-[1.5rem] text-[var(--lightBlue)]">Articles</h1>
       </div>  */}
       <div className="w flex items-center justify-center py-8 ">
-        <h2 className="text-[1.5rem] text-center">Chech out my Articles</h2>
+        <h2 className="text-[1.5rem] text-center">Check out my Articles</h2>
       </div>
       <div className="w m-auto flex items-center justify-center">
         <button className="font-normal text-[1.3rem] text-[#D8D8D8] bg-[#081942] py-3 px-16 mr-40">
