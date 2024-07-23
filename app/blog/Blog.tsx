b21d78ecd93e83244e855abeb9515f8e9954e90a
AlanTyping <alan.a.n.r.ar@gmail.com>
2024-07-23 18:30:00 -0300

    .

--- app/blog/Blog.tsx
+++ app/blog/Blog.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 import Image from "next/image";
 import BookPhone from './utils/book-phone.svg';
 import BookPc from './utils/book-pc.svg';
-import { prata } from "../utilities/fonts";
+import { prata, poppins700 } from "../utilities/fonts";
 
 async function getPosts() {
   const posts = getAllPosts()
@@ -17,7 +17,7 @@ export default async function Blog() {
   const posts = await getPosts();
 
   return (
-    <div className={`text-white bg-[#040D23] pb-16 ${prata.className} w flex items-center flex-col justify-center`}>
+    <div className={`py-10 text-white bg-[#040D23] pb-16 ${prata.className} w flex items-center flex-col justify-center`}>
       {/* <div className="w text-start">
         <Link href="/">
         <h1 className="text-[1.2rem] font-bold">Back home</h1>
@@ -26,11 +26,11 @@ export default async function Blog() {
       <div className="w-[60%] m-auto">
         <h1 className="font-bold text-[1.5rem] text-[var(--lightBlue)]">Articles</h1>
       </div>  */}
-      <div className="w flex items-center justify-center lg:justify-start lg:w-[1000px] py-8">
-        <h2 className="text-[1.5rem] text-center text-[#b3b3b3]">Articles</h2>
+      <div className="w-[90%] flex lg:w-[1000px] pb-12">
+        <h2 className={`text-[2rem] text-[var(--text)] ${poppins700.className}`}>Blog</h2>
       </div>
       <div className="w m-auto flex items-center justify-center">
-        <button className="font-normal text-[1.3rem] text-[#b7b7b7] bg-[#05112d] py-3 px-16 mr-40 lg:mb-[-10px]">
+        <button className="font-normal text-[1.3rem] text-[var(--text)] bg-[#05112d] py-3 px-16 mr-40 lg:mb-[-10px]">
           Filtrar
         </button>
       </div>
--- app/components/SchoolSection.tsx
+++ app/components/SchoolSection.tsx
@@ -5,13 +5,13 @@ import React from 'react';
 export default function SchoolSection() {
   return (
     <div className="py-16 2xl:py-20 text-[0.95rem] z-40 lg:mt-[-120px] xl:mt-[-16px] md:text-[1.1rem] 2xl:text-[1.2rem] flex-center flex-col justify-center w bg-[var(--secondary)] border-y border-[var(--tertiary)] text-white">
-      <p className='md:w-[60%] w-[80%]'>
+      <p className='md:w-[60%] w-[90%]'>
         Desarrollo de páginas
         web para instituciones educativas 🏫, por lo que, si formas parte
         de un colegio que necesita una página web o actualizar la
         que ya tiene, quizás te sientas interesado.
       </p>
-      <div className='md:w-[60%] w-[80%] mt-4'>
+      <div className='md:w-[60%] w-[90%] mt-4'>
         <span>Para más info, consultar el siguiente pdf haciendo <a className='text-[var(--lighterBlue)] border-b-[1px] border-[var(--lighterBlue)]' href="./servicio.pdf" download>click aquí</a></span>
       </div>
     </div>
