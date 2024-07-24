aae1766145432131ed3c34de1c17943b66dfaf02
AlanTyping <alan.a.n.r.ar@gmail.com>
2024-07-23 21:58:35 -0300

    adding a really cool animation

--- app/components/SchoolSection.tsx
+++ app/components/SchoolSection.tsx
@@ -1,19 +1,55 @@
 'use client'
 
-import React from 'react';
+import { useEffect, useRef } from 'react';
+import { motion, useAnimation } from 'framer-motion';
+
 
 export default function SchoolSection() {
+  const animatedElementRef = useRef(null);
+  const controls = useAnimation();
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            controls.start({ height: 250 }); // Agregar clase de animación al entrar en la vista
+          } else {
+            // Eliminar clase de animación al salir de la vista
+          }
+        });
+      },
+      { threshold: 0.1 } // Umbral del 75% de visibilidad
+    );
+
+    if (animatedElementRef.current) {
+      observer.observe(animatedElementRef.current);
+    }
+
+    return () => {
+      if (animatedElementRef.current) {
+        observer.unobserve(animatedElementRef.current);
+      }
+    };
+  }, []);
+
   return (
-    <div className="py-16 px-[27px] 2xl:py-20 text-[0.95rem] z-40 lg:mt-[-120px] xl:mt-[-16px] md:text-[1.1rem] 2xl:text-[1.2rem] flex-center flex-col justify-center w bg-[var(--secondary)] border-y border-[var(--tertiary)] text-white">
-      <p className='md:w-[60%] w'>
+    <motion.div
+      initial={{ height: 0 }} // Estado inicial fuera de la vista
+      animate={controls} // Animación controlada por Framer Motion
+      transition={{ duration: 2, ease: 'easeOut' }}
+      ref={animatedElementRef}
+      className="overflow-hidden px-[27px] 2xl:py-20 text-[0.95rem] z-40 lg:mt-[-120px] xl:mt-[-16px] md:text-[1.1rem] 2xl:text-[1.2rem] flex-center flex-col justify-center w bg-[var(--secondary)] border-y border-[var(--tertiary)] text-white">
+      <motion.p
+        className='md:w-[60%] w'>
         Desarrollo de páginas
         web para instituciones educativas 🏫, por lo que, si formas parte
         de un colegio que necesita una página web o actualizar la
         que ya tiene, quizás te sientas interesado.
-      </p>
-      <div className='md:w-[60%] w mt-4'>
+      </motion.p>
+      <motion.div className='md:w-[60%] w mt-4'>
         <span>Para más info, consultar el siguiente pdf haciendo <a className='text-[var(--lighterBlue)] border-b-[1px] border-[var(--lighterBlue)]' href="./servicio.pdf" download>click aquí</a>.</span>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   )
 }
