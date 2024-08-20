b5eb518d94415919cabc9185100592886e8d78d7
AlanTyping <alan.a.n.r.ar@gmail.com>
2024-08-20 13:25:05 -0300

    .

--- app/components/SchoolSection.tsx
+++ app/components/SchoolSection.tsx
@@ -13,7 +13,7 @@ export default function SchoolSection() {
       (entries) => {
         entries.forEach((entry) => {
           if (entry.isIntersecting) {
-            controls.start({ height: 250 }); // Agregar clase de animación al entrar en la vista
+            controls.start({ height: 500 }); // Agregar clase de animación al entrar en la vista
           } else {
             // Eliminar clase de animación al salir de la vista
           }
@@ -39,28 +39,25 @@ export default function SchoolSection() {
       animate={controls} // Animación controlada por Framer Motion
       transition={{ duration: 1, ease: 'backOut', delay: 0.15 }}
       ref={animatedElementRef}
-      className="overflow-hidden overflow-y-scroll px-[27px] text-[0.95rem] z-40 lg:mt-[-120px] xl:mt-[-16px] md:text-[1.1rem] 2xl:text-[1.2rem] flex-center gap-4 flex-col justify-center w bg-[var(--secondary)] border-y border-[var(--tertiary)] text-white">
-      <motion.p className='md:w-[60%] w pt-[510px] md:pt-[250px]'>
-        Me llamo Alan, tengo 20 años y estoy buscando experiencias para
-        aumentar mi conocimiento y poder ser consultor educativo. Si estás leyendo
-        esto, probablemente seas parte de un colegio con el que me contacté y
-        sepas que estoy buscando una colaboración de par en par para
-        mejorar y crecer juntos. Estoy ofreciendo un servicio
-        único y gratuito de consultoría durante 3 meses, entendiendo que no soy
-        ningún profesional pero las ganas de crecer sobran, no puedo prometer
-        mejoras, pero sí aprendizaje.
+      className="overflow-hidden overflow-y-scroll gap-4 px-[27px] text-[0.95rem] z-40 lg:mt-[-120px] xl:mt-[-16px] md:text-[1.1rem] 2xl:text-[1.2rem] flex-center flex-col justify-center w bg-[var(--secondary)] border-y border-[var(--tertiary)] text-white">
+      <motion.p className='md:w-[60%] w pt-[130px] md:pt-[10px]'>
+        Me llamo Alan, tengo 20 años y estoy ofreciendo un servicio de aprendizaje único.
       </motion.p>
       <motion.p className='md:w-[60%] w'>
         Soy programador hace 3 años y tengo experiencia en desarrollo web, tanto del lado
-        del servidor como del lado del cliente. Sin embargo, me di cuenta de que no quiero
-        dedicarme a eso, sino usarlo como herramienta cuando se necesite.
+        del servidor como del lado del cliente. Sin embargo, me di cuenta de que no es lo que quiero.
       </motion.p>
       <motion.p
         className='md:w-[60%] w'>
-        Como mi experiencia en el sistema educativo fue muy mala, siento la necesidad
-        de ir más allá de los fundamentos y trabajar de par en par con colegios para
-        que podamos tener la mejor experiencia posible, y por ende aumentar
-        riquezas y prosperidad.
+        Como mi experiencia en el sistema educativo fue muy mala, siento el propósito
+        de ayudar a mejorarlo y que todos podamos tener la mejor experiencia posible,
+        ayudando a resolver problemas clave que generan reacciones negativas en cadena.
+        La gracia es enfrentarlos y que esas reacciones sean positivas.
+      </motion.p>
+      <motion.p
+        className='md:w-[60%] w'>
+        Debido a esto voy a ser consultor educativo, pero como no tengo experiencia, necesito aprender, y no se me ocurre
+        mejor manera que ofreciendo un servicio totalmente único Y gratuito durante 3 meses.
       </motion.p>
       <motion.div className='md:w-[60%] w pb-7'>
         <span>Para más info, consultar el siguiente pdf haciendo <a className='text-[var(--lighterBlue)] border-b-[1px] border-[var(--lighterBlue)]' href="./servicio.pdf" download>click aquí</a>.</span>
